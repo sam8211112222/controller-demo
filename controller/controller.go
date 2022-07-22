923e5e48de7c8330f5b71e52cf5dccee43d8b53e
Sam Chen <sam8211112222@gmail.com>
2022-07-22 15:21:04 +0800

    add form

--- controller/controller.go
+++ controller/controller.go
@@ -12,7 +12,10 @@ var (
 
 func Startup(templates map[string]*template.Template) {
 	homeController.homeTemplate = templates["home.html"]
+	homeController.standLocatorTemplate = templates["stand_locator.html"]
 	shopController.shopTemplate = templates["shop.html"]
+	shopController.categoryTemplate = templates["shop_details.html"]
+	shopController.productTemplate = templates["shop_detail.html"]
 	homeController.registerRoutes()
 	shopController.registerRoutes()
 	http.Handle("/img/", http.FileServer(http.Dir("public")))
--- controller/home.go
+++ controller/home.go
@@ -7,15 +7,22 @@ import (
 )
 
 type home struct {
-	homeTemplate *template.Template
+	homeTemplate         *template.Template
+	standLocatorTemplate *template.Template
 }
 
 func (h home) registerRoutes() {
-	http.HandleFunc("/home", h.handleHome)
 	http.HandleFunc("/", h.handleHome)
+	http.HandleFunc("/home", h.handleHome)
+	http.HandleFunc("/stand-locator", h.handleStandLocator)
 }
 
 func (h home) handleHome(w http.ResponseWriter, r *http.Request) {
 	vm := viewmodel.NewHome()
 	h.homeTemplate.Execute(w, vm)
+}
+
+func (h home) handleStandLocator(w http.ResponseWriter, r *http.Request) {
+	vm := viewmodel.NewStandLocator()
+	h.standLocatorTemplate.Execute(w, vm)
 }
